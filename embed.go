db11c0b41587ea92e3814331a1b8670dc15e626f
erniealice <16433795+erniealice@users.noreply.github.com>
2026-03-10 20:49:06 +0800

    20260310: Rename translation files to singular, add professional business type

--- embed.go
+++ embed.go
@@ -6,4 +6,5 @@ import "embed"
 //go:embed translations/en/general/*.json
 //go:embed translations/en/retail/*.json
 //go:embed translations/en/service/*.json
+//go:embed translations/en/professional/*.json
 var TranslationsFS embed.FS
